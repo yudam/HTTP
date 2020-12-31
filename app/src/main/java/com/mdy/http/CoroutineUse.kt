4028b5cd73f806284e60142e5dc6da3cf282efad
maodayu <maodayu@sangame.com>
2020-12-31 17:42:03 +0800

    协程

--- app/src/main/java/com/mdy/http/CoroutineUse.kt
+++ app/src/main/java/com/mdy/http/CoroutineUse.kt
@@ -1,30 +1,89 @@
 package com.mdy.http
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.withContext
+import android.util.Log
+import kotlinx.coroutines.*
 
 /**
  * User: maodayu
  * Date: 2020/12/29 18:56
  */
 
-/**
- * 协程：本质是切换线程，完成后自动切换到开始线程
- */
+fun testScope() {
+    val scope = CoroutineScope(Dispatchers.Main)
+    Log.i("MDY", "start:${Thread.currentThread().name}")
+    scope.launch {
+        delay(4000)
+        Log.i("MDY", "scope:${Thread.currentThread().name}")
 
-suspend fun testSuspendIO(){
-    withContext(Dispatchers.IO){}
-}
-suspend fun testSuspendMAIN(){
-    withContext(Dispatchers.Main){}
+    }
+    Thread.sleep(2000)
+    Log.i("MDY", "end:${Thread.currentThread().name}")
+
+    runBlocking {
+
+    }
 }
 
-suspend fun testSuspendDefault(){
-    withContext(Dispatchers.IO){}
+fun testGlobalScope() {
+    Log.i("MDY", "start:${Thread.currentThread().name}")
+    GlobalScope.launch(Dispatchers.Main) {
+
+        launch {
+            delay(1000)
+            Log.i("MDY", "coroutineA:${Thread.currentThread().name}")
+        }
+
+        launch {
+            delay(2000)
+            Log.i("MDY", "coroutineB:${Thread.currentThread().name}")
+        }
+
+        Log.i("MDY", "coroutineC:${Thread.currentThread().name}")
+    }
+    Log.i("MDY", "end:${Thread.currentThread().name}")
+    Thread.sleep(500)
 }
 
-suspend fun testSuspendUnconfined(){
-    withContext(Dispatchers.Unconfined){}
+fun testRunBlocking() {
+    Log.i("MDY", "start:${Thread.currentThread().name}")
+
+    runBlocking {
+
+        launch {
+            repeat(3) {
+                delay(1000)
+                Log.i("MDY", "coroutineA:${Thread.currentThread().name}:" + it)
+            }
+        }
+
+        launch {
+            repeat(3) {
+                delay(2000)
+                Log.i("MDY", "coroutineB:${Thread.currentThread().name}:" + it)
+            }
+
+        }
+
+        Log.i("MDY", "coroutineC--runBlocking:${Thread.currentThread().name}")
+    }
+
+    Log.i("MDY", "end:${Thread.currentThread().name}")
+
 }
+
+fun testRunBlocking2(){
+    Log.i("MDY", "start:${Thread.currentThread().name}")
+    GlobalScope.launch {
+        delay(200)
+        Log.i("MDY", "GlobalScope:${Thread.currentThread().name}")
+    }
+    runBlocking {
+        launch {
+            delay(1000)
+            Log.i("MDY", "coroutineA:${Thread.currentThread().name}")
+        }
+        Log.i("MDY", "coroutineC--runBlocking:${Thread.currentThread().name}")
+    }
+    Log.i("MDY", "end:${Thread.currentThread().name}")
+}
+
--- app/src/main/java/com/mdy/http/MainActivity.kt
+++ app/src/main/java/com/mdy/http/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.*
-import okhttp3.Response
-import java.util.*
-import kotlin.collections.ArrayDeque
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,25 +15,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel = NetViewModel(application)
         tv_content = findViewById(R.id.tv_content)
-        val scope = CoroutineScope(Dispatchers.Main+ Job())
-        val job = scope.launch {  }
-        job.cancel()
-        scope.cancel()
-        scope.async {  }
     }
 
-
-    fun sendHttp(view: View) {
-        viewModel.getData(object : OnHttpResultCallback {
-            override fun onSuccess(result: Response) {
-                runOnUiThread{
-                    tv_content.text = result.body?.string() ?: "this is null"
-                }
-            }
-
-            override fun onFailed(throwable: Throwable) {
-            }
-        })
+     fun sendHttp(view: View) {
+         testRunBlocking2()
+//        viewModel.getData(object : OnHttpResultCallback {
+//            override fun onSuccess(result: Response) {
+//                runOnUiThread{
+//                    tv_content.text = result.body?.string() ?: "this is null"
+//                }
+//            }
+//
+//            override fun onFailed(throwable: Throwable) {
+//            }
+//        })
     }
 
     fun clearHttp(view: View){
